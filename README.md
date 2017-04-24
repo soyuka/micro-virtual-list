bc6d8a23d6e20890520ad88e776490801ae134a5
abluchet <abluchet@ds-restauration.com>
2017-04-24 16:03:21 +0200

    init

---
+++ README.md
@@ -0,0 +1,64 @@
+Micro Virtual List
+==================
+
+Virtual lists with customizable heights on each elements.
+
+Usually on virtual list libraries you have to specify a fixed height. This module allows you to specify a height per row.
+
+## Installation
+
+```
+npm install micro-virtual-list --save
+```
+
+## Usage
+
+### Simple
+
+```javascript
+const container = document.getElementById('my-list')
+MicroVirtualList(container, {
+  height: 700, // desired container height
+  total: 10000, // total number of rows
+  itemHeight: 70, // a row height
+  getRow: (i) => {
+    const el = document.createElement('div')
+    el.innerText = 'Row: ' + i
+    return el
+  }
+})
+```
+
+### Dynamic heights
+
+```javascript
+const container = document.getElementById('foo')
+const total = 10000
+const itemHeight = 70 // must be the Max height for better results (Math.max.apply(null, heightsArray))
+
+// random heights
+const heights = new Array(total).fill(0).map((e) => Math.floor(Math.random() * (20 - 70) + 70))
+
+MicroVirtualList(container, {
+  total: total,
+  itemHeight: itemHeight,
+  height: 400,
+  getRow: (i) => {
+    const el = document.createElement('tbody')
+    const tr = document.createElement('tr')
+    const td = document.createElement('td')
+    td.innerText = 'Row: ' + i + ' ( '+heights[i]+'px )'
+
+    tr.appendChild(td)
+    el.appendChild(tr)
+
+    return {element: el, height: heights[i]}
+  }
+})
+```
+
+## References
+
+- https://github.com/tbranyen/hyperlist
+- https://github.com/sergi/virtual-list
+
---
+++ index.js
@@ -0,0 +1,125 @@
+function MicroVirtualList(container, config) {
+  // Set up container, block display has to be forced on tables
+  container.setAttribute('style', `width:100%;height:${config.height}px;overflow:auto;position:relative;padding:0;display:block;`)
+
+  const total = config.total
+
+  // Our shadow element to show a correct scroll bar
+  const scroller = document.createElement('tr');
+
+  // Cache heights
+  const heights = new Array(total).fill(0).map((e, i) => {
+    let row = config.getRow(i)
+
+    if (row.height !== undefined) {
+      return row.height
+    }
+
+    return config.itemHeight
+  })
+
+  // Compute positions
+  const positions = new Array(total).fill(0)
+
+  computePositions()
+
+  // Scroll height
+  const scrollHeight = computeScrollHeight()
+
+  // Visible elements
+  const averageHeight = scrollHeight / total
+  const visibleCache = Math.ceil(config.height / averageHeight) * 3
+
+  // stores the last scrollTop
+  let lastRepaint
+
+  render()
+
+  function computeScrollHeight() {
+    const scrollHeight = heights.reduce((a, b) => a + b, 0)
+
+    scroller.setAttribute('style', `opacity:0;position:absolute;width:1px;height:${scrollHeight}px;`)
+
+    return scrollHeight
+  }
+
+  function computePositions(from = 1) {
+    if (from < 1) {
+      from = 1
+    }
+
+    for (let i = from; i < total; i++) {
+      positions[i] = heights[i - 1] + positions[i - 1]
+    }
+  }
+
+  // Get the index we start from
+  function getFrom() {
+    const scrollTop = container.scrollTop
+    let i = 0
+
+    while (positions[i] < scrollTop) {
+      i++
+    }
+
+    return i
+  }
+
+  // Render elements
+  function render() {
+    const scrollTop = container.scrollTop
+
+    if (lastRepaint === scrollTop) {
+      return requestAnimationFrame(render)
+    }
+
+    let from = getFrom() - 1
+    from = from < 0 ? 0 : from
+
+    let to = from + visibleCache
+    to = to > total ? total : to
+
+    const fragment = document.createDocumentFragment()
+    fragment.appendChild(scroller)
+
+    for (let i = from; i < to; i++) {
+      let element = config.getRow(i)
+
+      if (element.height) {
+        if (heights[i] !== element.height) {
+          heights[i] = element.height
+          computePositions(i)
+          computeScrollHeight()
+        }
+
+        element = element.element
+      }
+
+      const top = positions[i]
+
+      element.setAttribute('style', `
+        ${element.style.cssText || ''}
+        position: absolute;
+        top: ${top}px;
+      `)
+
+      fragment.appendChild(element)
+    }
+
+    lastRepaint = scrollTop
+
+    container.innerHTML = ''
+    container.appendChild(fragment)
+    requestAnimationFrame(render)
+  }
+
+  function destroy() {
+    cancelAnimationFrame(render)
+  }
+
+  return {
+    destroy: destroy
+  }
+}
+
+module.exports = MicroVirtualList
