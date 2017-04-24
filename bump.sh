b7133ba6e37b0adb2e57600d88d2160cf002a34b
abluchet <abluchet@ds-restauration.com>
2017-04-24 16:06:44 +0200

    bump

---
+++ bump.sh
@@ -0,0 +1,26 @@
+#!env bash
+
+[[ '' == $1 ]] && echo "Please provide patch, minor, major argument" && exit 1
+
+tag='next'
+
+if [[ $1 == 'patch' || $1 == 'minor' || $1 == 'major' ]]; then
+        tag='latest'
+fi
+
+mkdir dist || echo
+
+npm run build
+npm run build-min
+newver=$(npm --no-git-tag-version version $1)
+git add -f dist package.json
+git commit -m $newver
+git tag $newver
+npm publish --tag $tag
+git reset --hard HEAD~1
+newver=$(npm --no-git-tag-version version $1)
+git add package.json
+git commit -m $newver
+git push --tags
+git push
+
